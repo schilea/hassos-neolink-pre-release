ed16d3b78f9b0597812def4c22cd15c0ff46bc92
schilea <57327794+schilea@users.noreply.github.com>
2024-01-16 22:53:35 +0200

    Update run.sh

--- neolink-prerelease/run.sh
+++ neolink-prerelease/run.sh
@@ -8,7 +8,7 @@ LOG=$(jq --raw-output '.log // empty' $CONFIG_PATH)
 
 echo "--- VERSIONS ---"
 echo "add-on version: 0.0.1"
-echo -n "neolink version: " && neolink --version
+echo -n "neolink pre-release version: " && neolink --version
 echo "neolink mode: ${MODE}"
 echo "neolink log: ${LOG}"
 echo "ATTENTION: if you expected a newer Neolink version, please reinstall this Add-on!"
